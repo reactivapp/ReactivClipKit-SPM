33e8a96be17155f94ba6e78df41a1136558df73b
Tim Sopin <tim@reactivapp.com>
2025-09-02 22:54:55 -0700

    Update to version 1.1.2509022

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ReactivClipKit",
-            url: "https://cdn.reactivapp.com/downloads/reactiv-clip-kit/ReactivClipKit-1.1.2509021.xcframework.zip",
-            checksum: "faa28c2ce36c2b72b21c40ebb70d69db92074b18c7e52d0491774ec52d35b6aa"
+            url: "https://cdn.reactivapp.com/downloads/reactiv-clip-kit/ReactivClipKit-1.1.2509022.xcframework.zip",
+            checksum: "96fa80e686dfb08b6a9fb847084acda8f78ffdc4df142723f377c11ffbd5caf1"
         )
     ]
 ) 
